877b8a07841ab16015ac81946c99cab20f6776c1
jruns <jruns@users.noreply.github.com>
2025-05-14 17:32:20 +0000

    2025 upgrades for Firefox (#1)

--- README.md
+++ README.md
@@ -2,7 +2,15 @@
 A simple browser extension notifying of a WordPress site in your current tab.
 
 ## Location installation instructions
-Coming soon
+1. Download a release zip file
+2. Go to about:addons
+3. Click the gear icon next to the "Manage Your Extensions" heading
+4. Select "Install Add-on From File..." and select the release file
+5. Pin the extension to the Toolbar
+
+## ToDo
+1. Correctly set the toolbar icon when multiple windows are open and focus switches between them.
+2. Find WordPress sites that miss the head check (like sites using a CDN), and add body checks to match those as well.
 
 ## Credits
 Forked from https://github.com/bensaine/wordpress-detector
--- src/background.js
+++ src/background.js
@@ -1,20 +1,21 @@
 'use strict';
 
 // terms we will search in <head> to detect WordPress
-const clues = ["wp-content", "wp-includes"]
-
-// reset icon
-updateIcon(false)
+const clues = {
+  head: [ "wp-content", "wp-includes", "xmlrpc.php" ]
+}
 
 // updates the icon in the toolbar
 const updateIcon = (state) => {
-  const path = state ? "icon_on_32.png" : "icon_off_32.png";
+  const path = state ? "icons/icon_on_32.png" : "icons/icon_off_32.png";
   chrome.browserAction.setIcon({ path })
 }
 
 // detect wordpress in a pages <head>
 const detectWordPress = (head) => {
-  updateIcon(head && head != null && head !== "" && clues.some(v => head.includes(v)))
+  if ( typeof head !== "undefined" ) {
+    updateIcon(head && head != null && head !== "" && clues.head.some(v => head.includes(v)))
+  }
 }
 
 const onTabUpdate = () => {
@@ -22,10 +23,12 @@ const onTabUpdate = () => {
   updateIcon(false)
 
   // get current tab's <head> and detect WordPress
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.sendMessage(tabs[0].id, { text: "send_head_inner" }, function (response) {
-      detectWordPress(response)
-    });
+  chrome.tabs.query({ active: true, currentWindow: true, url: '*://*/*' }, function (tabs) {
+    if ( tabs.length !== 0 ) {
+      chrome.tabs.sendMessage(tabs[0].id, { text: "send_head_inner" }, function (response) {
+        detectWordPress(response)
+      });
+    }
   });
 }
 
